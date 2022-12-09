8b340fb1dd41185f32db1d154c595ee4d123eec2
efuru <efuruanthonytech@yahoo.com>
2022-12-09 14:28:45 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if output is correct
+ */
+
+int main(void)
+
+{
+	char str1[] ="\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+        return (0);
+}
