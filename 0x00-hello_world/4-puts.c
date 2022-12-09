a9109806ce641745961ed42092fef0b5b8bc4f1d
efuru <efuruanthonytech@yahoo.com>
2022-12-09 14:48:43 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,11 +7,10 @@
  */
 
 int main(void)
-
 {
-	char str1[] ="\"Programming is like building a multilingual puzzle";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
-
-        return (0);
+	
+	return (0);
 }
