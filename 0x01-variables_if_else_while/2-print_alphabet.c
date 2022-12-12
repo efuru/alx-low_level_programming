e6f3a6299cbd751a43a167cb11057fdb9fbe5da4
efuru <efuruanthonytech@yahoo.com>
2022-12-12 14:48:09 -0800

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - program prints the alphabet in lowercase
  *
  * Description: this program prints the alphabet in
@@ -11,17 +10,14 @@
  */
 
 int main(void)
-
 {
 	int n = 97;
-	
+
 	while (n < 123)
 	{
 		putchar(n);
 		n++;
 	}
-	
 	putchar(10);
-	
 	return (0);
 }
