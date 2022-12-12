21ecb3c410d1f2a16a8879f5604416ba2bef92a7
efuru <efuruanthonytech@yahoo.com>
2022-12-12 15:29:44 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print alphabet followed by new line
+ *
+ * Description: This program prints alphabet in lowercase and uppercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+	int e, f;
+	
+	e = 97;
+	f = 65;
+
+	while (e < 123)
+	{
+		putchar(e);
+		e++;
+	}
+
+	while (f < 65)
+	{
+		putchar(f);
+		f++;
+	}
+	
+	putchar(10);
+
+	return (0);
+}
