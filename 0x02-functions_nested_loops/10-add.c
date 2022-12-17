72614e6d820381ba18516b804cec9f74c0e6ffcb
efuru <efuruanthonytech@yahoo.com>
2022-12-17 05:19:13 -0800

    10-main.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: integer 1
+ * @l: inter 2
+ *
+ * Description: give two intergers, returns e
+ *
+ * Return: e
+ */
+
+int add(int a, int l)
+{
+	int e;
+
+	e = a + l;
+
+	return (e);
+}
