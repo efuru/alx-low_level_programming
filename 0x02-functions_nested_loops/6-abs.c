dba9085f65a5f3947f70a221640f2b26c2f199f1
efuru <efuruanthonytech@yahoo.com>
2022-12-17 01:54:47 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - the absolute value of an integer
+ *
+ * @n: integer to be computed
+ *
+ * Return: absolute value of an integer
+ */
+
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('n');
+	}
+		return (-n);
+}
+
