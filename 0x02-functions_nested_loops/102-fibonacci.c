aa728c7983b73d909f9f4de451fc9fe9ac086d39
efuru <efuruanthonytech@yahoo.com>
2022-12-18 04:50:23 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int counter;
+	int countto = 50;
+	long a = 1;
+	long b = 2;
+
+	for (counter = 1; counter <= (countto / 2); counter++)
+	{
+		printf("%li %li ", a, b);
+		a += b;
+		b += a;
+	}
+	if (countto % 2 == 1)
+		printf("%li", a);
+	printf("\n");
+	return (0);
+}
