b64d4798ca653a0f3d9d2161736a25cac4e7c3b5
efuru <efuruanthonytech@yahoo.com>
2022-12-18 15:13:44 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
