7a6c4985c5d25df4137b671a5320b219a05d6e52
efuru <efuruanthonytech@yahoo.com>
2022-12-18 02:40:00 -0800

    11-print_to_98.correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,19 +13,20 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n < 98)
 	{
 		printf("%d, ", n);
 		n++;
 	}
 
-	if (n > 98)
+	while (n > 98)
 	{
 		printf("%d, ", n);
 		n--;
 	}
 
-	printf("98\n");
+	if (n == 98)
+		printf("%d", n);
+
+	printf("\n");
 }
-
-
