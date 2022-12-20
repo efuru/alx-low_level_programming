47b8356a01708ab2a2cc5d6ef784e280123660d9
efuru <efuruanthonytech@yahoo.com>
2022-12-20 07:58:39 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value
+ *
+ * the pointer points to 98
+ *
+ * @n: pointer of the variable to be updated
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+This directory contains my projects on pointers and array including strings.
+
+C is my first programming landguage and I'm doing my best to understand 
+
+so much in so short a period of time.
