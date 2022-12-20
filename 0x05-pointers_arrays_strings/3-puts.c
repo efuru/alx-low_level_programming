5a42f610618ca32081abf366e5866ab556516a4c
efuru <efuruanthonytech@yahoo.com>
2022-12-20 10:34:53 -0800

    putchar

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,11 @@
 
 void _puts(char *str)
 {
-	int i = '0';
+	int i;
 
-	for (i = '0'; str[i] != '\0'; str[i]++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		putchar(str[i]);
+		_putchar(str[i]);
 	}
-	
-	putchar('\n');
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int i = 0;
 	int j;
 
-	while (*s != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 		++s;
