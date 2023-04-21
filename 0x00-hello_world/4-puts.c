2cfaab38b4fd597fae2bebcda3aa9297dd152955
efuru <efuruanthonytech@yahoo.com>
2023-04-21 03:22:20 -0700

    4- Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-<include stdio.h>
+#include <stdio.h>
 /**
  * main - Entry piont
  * Return: 0 successful
@@ -7,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return(0);
+	return(0);
 }
