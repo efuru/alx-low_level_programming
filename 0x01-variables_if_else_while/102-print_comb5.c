7b17e1e1babe7b184fbc055fd1f09a78d824b2f0
efuru <efuruanthonytech@yahoo.com>
2023-04-23 12:41:54 -0700

    102, check 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
 
 /**
- * main - program prints all possible different combinations of three digits
+ * main - program prints all possible combinations of two two-digit numbers
  *
- * Description: prints all possible different combinations of three digits
+ * Description: prints all possible different combinations of two-digit numbers
  *
- * Return: 0
+ * Return: Always 0 (success)
  *
  */
 
 int main(void)
+{
+	int e, f, g, h;
+
+	e = 48;
+
+	while (e < 58)
+	{
+		f = e + 1;
+
+		while (f < 58)
+		{
+			g = f + 1;
+
+			while (g < 58)
+			{
+				h = g + 1;
+
+				while (h < 58)
+				{
+					putchar(e);
+					putchar(f);
+					putchar(32);
+					putchar(g);
+					putchar(h);
+
+					if (e < 57 || f < 56 || g < 57 || h < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					h++;
+				}
+				g++;
+			}
+			f++;
+		}
+		e++;
+	}
+	putchar(10);
+
+	return (0);
+}
