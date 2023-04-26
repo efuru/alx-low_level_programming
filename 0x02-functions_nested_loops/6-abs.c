075f2ab08f9ead8cc9437cc1c9b0457242368753
efuru <efuruanthonytech@yahoo.com>
2023-04-26 13:04:19 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolut value of an integer
+ *
+ * @e: integer to be computed
+ *
+ * Return: absolute value of an integer
+ *
+ */
+
+int _abs(int e)
+{
+	if (e < 0)
+	{
+		_putchar(e);
+
+		return (-e);
+	}
+	else if (e > 0)
+	{
+		_putchar(e);
+
+		return (e);
+	}
+	else
+	{
+		return (e);
+	}
+}
