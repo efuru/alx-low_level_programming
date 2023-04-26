cf6b896f4b2183ccbbbb452a691bfa19fd9c59e6
efuru <efuruanthonytech@yahoo.com>
2023-04-26 11:11:16 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _islower - cheacks for the lowercase character
+ *
+ * @c: character to check
+ *
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+
+	{
+		_putchar(c);
+
+		return (1);
+	}
+	else
+	{
+		_putchar(c);
+
+		return (0);
+	}
+}
