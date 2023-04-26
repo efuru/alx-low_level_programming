9c93d5c91f5c8b5b223f5537105cfae1ad7213f6
efuru <efuruanthonytech@yahoo.com>
2023-04-26 06:59:44 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  alphabet, in lowercase, followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char e;
+	int a;
+
+	a = 0;
+
+	while (a <= 10)
+	{
+		e = 'a';
+		while (e <= 'z')
+		{
+			_putchar(e);
+			e++;
+		}
+
+		_putchar('\n');
+		a++;
+	}
+}
