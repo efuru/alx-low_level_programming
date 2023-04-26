ebd987c3fdbc99235441d05c9d85af5c5e146c12
efuru <efuruanthonytech@yahoo.com>
2023-04-25 21:31:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints _putchar
+ *
+ * Description: This is a program that prints _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,9 @@
+This directory contains my study
+
+on C function, C header files
+
+And nested loops including but not limited to
+
+Nested while loops, nested for loops and 
+
+Nested "Other types of" loops 
