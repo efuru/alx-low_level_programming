aef3be4567a434698acf9937cbf894ab401f0d92
efuru <efuruanthonytech@yahoo.com>
2023-05-25 10:57:08 -0700

    poppy is cute

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Nmae: (nil)\n");
+			printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
