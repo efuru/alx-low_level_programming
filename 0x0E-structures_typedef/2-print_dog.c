e986d602e1097250262385de915d63975f1cca85
efuru <efuruanthonytech@yahoo.com>
2023-05-25 09:01:20 -0700

    Poppy is cute!

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints struct for dog
+ * @d: pointer to dog strcture
+ *
+ * Description: this function prints structure fo dog
+ * Return: nil if element of d is null, nothing is d is null
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Nmae: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
