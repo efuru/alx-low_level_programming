f14104e67896ff0c677ac961b0b0acf1b944eff4
efuru <efuruanthonytech@yahoo.com>
2023-05-28 17:06:58 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,7 +12,6 @@
 int main(int argc, char *argv[])
 {
 	int i, num_byt;
-	unsigned char *pointer;
 
 	if (argc != 2)/*checks if argc is correct when the user types*/
 	{
@@ -25,13 +24,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
-	/*print opcode; use malloc to access main's memory*/
-	pointer = (unsigned char *)main;
-
 	for (i = 0; i < num_byt; i++)
 	{
-		printf("%02x ", pointer[i]);/*for hex, for space*/
+		printf("%02hhx", *((char *)main + 1));/*for hex, for space*/
+		if (i < num_byt - 1)
+		{
+			printf(" ");
+		}
+		printf("\n");/*for new line*/
 	}
-	printf("\n");/*for new line*/
 	return (0);
 }
